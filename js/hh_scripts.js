a6829f033ce8ebce23c2631c3722c283a86ec0b7
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-08-19 14:01:50 +0800

    remove extra spacing in js codes

--- js/hh_scripts.js
+++ js/hh_scripts.js
@@ -8,35 +8,32 @@ $(document).ready(function(){
     loadCourses();
     
     $("#accordion").accordion({ collapsible: true, active: 3 });
-    $("#main_display").scroll(detatchedElement);
     $("body")
-            .on('click',"#btn_add_course, #btn_cancel_add_course",function () {
-                $("#course_form").toggleClass("show");       
-            })
-            .on("submit","#course_form",submitCourseForm)
-            .on("click", ".courses", function(){
-                let course_id = $(this).val();
-
-                for(let index = 0; index < courses_data.length; index++){
-                    if(courses_data[index].id == course_id){
-                        courses_selected.push(courses_data[index]);
-                    }
-                }
-
-                console.log(courses_selected);
-            });
+            .on('click',"#btn_add_course, #btn_cancel_add_course", function () { $("#course_form").toggleClass("show"); })
+            .on("submit", "#course_form", submitCourseForm)
+            .on("click", ".courses", checkCourse);
     
 });
+
+function checkCourse(){
+    let course_id = $(this).val();
+
+    for(let index = 0; index < courses_data.length; index++){
+        if(courses_data[index].id == course_id){
+            courses_selected.push(courses_data[index]);
+        }
+    }
+}
 
 function submitCourseForm(){
     let course_form = $(this);  
     let snackbar =  $("#snackbar");  
     
+    $("#add_course_image").hide();
     course_form.trigger("reset");    
     snackbar.addClass("show");
     setTimeout(function(){ snackbar.removeClass("show"); }, 3000);
-    $("#add_course_image").hide();
-
+    
     loadAddedCourses();
     
     return false;
@@ -47,7 +44,7 @@ function loadAddedCourses(){
     $("#courses_list").html(html);
 }
 
-function addedCourseTemplate(courses) {
+function addedCourseTemplate(courses){
     let html_template = ``;
     
    for(let index = 0; index < courses.length; index++) {
@@ -59,56 +56,14 @@ function addedCourseTemplate(courses) {
         for(let assignment = 0; assignment < courses[index].assignments.length; assignment++) {
             html_template += `           <tr>`;
             html_template += `               <td>${courses[index].assignments[assignment]}</td>`;
+            
             for(let student = 0; student < students_data.length; student++) {
                 html_template += `            <td>--</td>`;
             }
-            // html_template += `               <td>--</td>`;
-            // html_template += `               <td>--</td>`;
-            // html_template += `               <td>--</td>`;
-            // html_template += `               <td>--</td>`;
-            // html_template += `               <td>--</td>`;
-            // html_template += `               <td>--</td>`;
+            
             html_template += `           </tr>`;
         }
-        // html_template += `           <tr>`;
-        // html_template += `               <td>Algorithm I</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `           </tr>`;
-        // html_template += `           <tr>`;
-        // html_template += `               <td>Algorithm I</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `           </tr>`;
-        // html_template += `           <tr>`;
-        // html_template += `               <td>Algorithm I</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `           </tr>`;
-        // html_template += `           <tr>`;
-        // html_template += `               <td>Algorithm I</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `               <td>--</td>`;
-        // html_template += `           </tr>`;
+
         html_template += `         </tbody>`;
         html_template += `   </table>`;
         html_template += `</div>`;
@@ -117,33 +72,30 @@ function addedCourseTemplate(courses) {
    return html_template;
 }
 
-function loadCourses() {
+function loadCourses(){
     let html = ``;
     
     for(let index = 0; index < courses_data.length; index++){
         html += `<li>`; 
-        html += `<input type="checkbox" class="courses" name="courses" value="${courses_data[index].id}"> ${courses_data[index].course_title}`   
+        html += `   <input type="checkbox" class="courses" name="courses" value="${courses_data[index].id}"> ${courses_data[index].course_title}`   
         html += `</li>`;
     }
 
     $("#course_list").html(html);   
 }
 
-async function getCountry(country) {
+async function getCountry(country){
     try {
         const response = await axios.get(`https://restcountries.eu/rest/v2/name/${country}`);
         
         return response.data[0].flag;
 
-    } catch (error) {
-        console.error(error);
-    }
+    } catch (error){}
 
 }
 
-async function loadStudents() {
+async function loadStudents(){
     try {
-
         let html = `<th>Assignments</th>`;
         
         for(let index = 0; index < students_data.length; index++){
@@ -153,17 +105,5 @@ async function loadStudents() {
         }
         $("#student_row").html(html);
 
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-function detatchedElement(){
-    
-    if($("#main_display").scrollLeft() > 5) {
-        $("#assignments p").addClass("assigment_scrolling");
-    }
-    else {
-        $("#assignments p").removeClass("assigment_scrolling");
-    }
+    } catch (error){}
 }
