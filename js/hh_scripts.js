f5002403531f28e5e19c449d3639ab29f992a7a6
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-08-19 16:35:34 +0800

    change loop variable into meaning full varialbe

--- js/hh_scripts.js
+++ js/hh_scripts.js
@@ -18,59 +18,14 @@ $(document).ready(function(){
 function checkCourse(){
     let course_id = $(this).val();
 
-    for(let index = 0; index < courses_data.length; index++){
-        if(courses_data[index].id == course_id){
-            courses_selected.push(courses_data[index]);
+    for(let course = 0; course < courses_data.length; course++){
+        if(courses_data[course].id == course_id){
+            courses_selected.push(courses_data[course]);
         }
     }
 }
 
-function submitCourseForm(){
-    let course_form = $(this);  
-    let snackbar =  $("#snackbar");  
-    
-    $("#add_course_image").hide();
-    course_form.trigger("reset");    
-    snackbar.addClass("show");
-    setTimeout(function(){ snackbar.removeClass("show"); }, 3000);
-    
-    loadAddedCourses();
-    
-    return false;
-}
 
-function loadAddedCourses(){
-    let html = addedCourseTemplate(courses_selected);
-    $("#courses_list").html(html);
-}
-
-function addedCourseTemplate(courses){
-    let html_template = ``;
-    
-   for(let index = 0; index < courses.length; index++) {
-        html_template += `<div class="course">`;
-        html_template += `   <h5>${courses[index].course_title}</h5>`;
-        html_template += `   <table>`;
-        html_template += `       <tbody>`;
-      
-        for(let assignment = 0; assignment < courses[index].assignments.length; assignment++) {
-            html_template += `           <tr>`;
-            html_template += `               <td>${courses[index].assignments[assignment]}</td>`;
-            
-            for(let student = 0; student < students_data.length; student++) {
-                html_template += `            <td>--</td>`;
-            }
-            
-            html_template += `           </tr>`;
-        }
-
-        html_template += `         </tbody>`;
-        html_template += `   </table>`;
-        html_template += `</div>`;
-   }
-
-   return html_template;
-}
 
 function loadCourses(){
     let html = ``;
@@ -98,12 +53,60 @@ async function loadStudents(){
     try {
         let html = `<th>Assignments</th>`;
         
-        for(let index = 0; index < students_data.length; index++){
-            html += `<th class="tooltip_utility">${students_data[index].last_name}, ${students_data[index].first_name[0]}... 
-                <span class="tooltip_utility_text"><img src="${await getCountry(students_data[index].country)}" /> ${students_data[index].last_name}, ${students_data[index].first_name}</span>
+        for(let student = 0; student < students_data.length; student++){
+            html += `<th class="tooltip_utility">${students_data[student].last_name}, ${students_data[student].first_name[0]}... 
+                <span class="tooltip_utility_text"><img src="${await getCountry(students_data[student].country)}" /> ${students_data[student].last_name}, ${students_data[student].first_name}</span>
             </th>`;
         }
         $("#student_row").html(html);
 
     } catch (error){}
+}
+
+
+function submitCourseForm(){
+    let course_form = $(this);  
+    let snackbar =  $("#snackbar");  
+    
+    $("#add_course_image").hide();
+    course_form.trigger("reset");    
+    snackbar.addClass("show");
+    setTimeout(function(){ snackbar.removeClass("show"); }, 3000);
+    
+    loadAddedCourses();
+    
+    return false;
+}
+
+function loadAddedCourses(){
+    let html = addedCourseTemplate(courses_selected);
+    $("#courses_list").html(html);
+}
+
+function addedCourseTemplate(courses){
+    let html_template = ``;
+    
+   for(let course = 0; course < courses.length; course++) {
+        html_template += `<div class="course">`;
+        html_template += `   <h5>${courses[course].course_title}</h5>`;
+        html_template += `   <table>`;
+        html_template += `       <tbody>`;
+      
+        for(let assignment = 0; assignment < courses[course].assignments.length; assignment++) {
+            html_template += `           <tr>`;
+            html_template += `               <td>${courses[course].assignments[assignment]}</td>`;
+            
+            for(let student = 0; student < students_data.length; student++) {
+                html_template += `            <td>--</td>`;
+            }
+            
+            html_template += `           </tr>`;
+        }
+
+        html_template += `         </tbody>`;
+        html_template += `   </table>`;
+        html_template += `</div>`;
+   }
+
+   return html_template;
 }
