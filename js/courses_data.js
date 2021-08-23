bf9a27a6930f363549a7053558ce821fed5d14e6
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-08-23 11:08:35 +0800

    code refactoring

--- js/courses_data.js
+++ js/courses_data.js
@@ -2,67 +2,67 @@ export default [
     {
         "id" : 1,
         "course_title" : "Learn to Code",
-        "selected" : true,
+        "is_selected" : false,
         "assignments" : ["Algorithm I", "Algorithm II", "Algorithm III", "Algorithm IV", "Algorithm V"]
     },
     {
         "id" : 2,
         "course_title" : "Advanced Javascript (beta)",
-        "selected" : true,
+        "is_selected" : false,
         "assignments" : ["Algorithm I", "Algorithm II", "Algorithm III", "Advanced Objects", "Higher Order Functions"]
     },
     {
         "id" : 3,
         "course_title" : "Games in Javascript",
-        "selected" : true,
+        "is_selected" : false,
         "assignments" : ["Pacman", "Super Mario"]
     },
     {
         "id" : 4,
         "course_title" : "Adv. Algorithm & Data Structures",
-        "selected" : true,
+        "is_selected" : false,
         "assignments" : ["Algorithm I", "Algorithm II", "Algorithm III", "Algorithm IV", "Algorithm V"]
     },
     {
         "id" : 5,
         "course_title" : "Basic Algorithm",
-        "selected" : true,
+        "is_selected" : false,
         "assignments" : ["Algorithm I", "Algorithm II", "Algorithm III", "Algorithm IV", "Algorithm V"]
     },
     {
         "id" : 6,
         "course_title" : "System Design (beta)",
-        "selected" : true,
+        "is_selected" : false,
         "assignments" : ["Test I", "Test II", "Algorithm III", "Algorithm IV", "Algorithm V"]
     },
     {
         "id" : 7,
         "course_title" : "Communication",
-        "selected" : true,
+        "is_selected" : false,
         "assignments" : ["Algorithm I", "Algorithm II", "Algorithm III", "Algorithm IV", "Algorithm V"]
     },
     {
         "id" : 8,
         "course_title" : "How to create an effective course",
-        "selected" : true,
+        "is_selected" : false,
         "assignments" : ["Algorithm I", "Algorithm II", "Algorithm III", "Algorithm IV", "Algorithm V"]
     },
     {
         "id" : 9,
         "course_title" : "User Experience",
-        "selected" : true,
+        "is_selected" : false,
         "assignments" : ["Algorithm I", "Algorithm II", "Algorithm III", "Algorithm IV", "Algorithm V"]
     },
     {
         "id" : 10,
         "course_title" : "Python",
-        "selected" : true,
+        "is_selected" : false,
         "assignments" : ["Algorithm I", "Algorithm II", "Algorithm III", "Algorithm IV", "Algorithm V"]
     },
     {
         "id" : 11,
         "course_title" : "MEAN Stack",
-        "selected" : true,
+        "is_selected" : false,
         "assignments" : ["MongoDB", "Mongoose", "MySQL", "Express", "Angular"]
     },
     
--- js/hh_scripts.js
+++ js/hh_scripts.js
@@ -1,11 +1,10 @@
 import students_data from "./students_data.js";
 import courses_data from "./courses_data.js";
 
-let courses_selected = [];
-
 $(document).ready(function(){
-    loadStudents();
+    loadAddedCourses();
     loadCourses();
+    loadStudents();
     
     $("#accordion").accordion({ collapsible: true, active: 3 });
     $("body")
@@ -17,12 +16,9 @@ $(document).ready(function(){
 
 function checkCourse(){
     for(let course = 0; course < courses_data.length; course++){
-        if(courses_data[course].id == $(this).val()){
-            courses_selected.push(courses_data[course]);
-        }
+        if(courses_data[course].id == $(this).val()) courses_data[course].is_selected = !courses_data[course].is_selected;    
     }
 }
-
 
 
 function loadCourses(){
@@ -31,7 +27,7 @@ function loadCourses(){
     for(let index = 0; index < courses_data.length; index++){
         html_template += `<li>`; 
         html_template += `   <label>`;
-        html_template += `      <input type="checkbox" class="courses" name="courses" value="${courses_data[index].id}">`;
+        html_template += `      <input type="checkbox" class="courses" name="courses" ${(courses_data[index].is_selected) ? "checked" : ""} value="${courses_data[index].id}">`;
         html_template += `      <span></span>`;
         html_template += `      <p>${courses_data[index].course_title}</p>`;
         html_template += `   </label>`;   
@@ -78,37 +74,41 @@ function submitCourseForm(){
     setTimeout(function(){ snackbar.removeClass("show"); }, 3000);
     
     loadAddedCourses();
-    
+    loadCourses();
     return false;
 }
 
 function loadAddedCourses(){
-    $("#courses_list").html(addedCourseTemplate(courses_selected));
+    $("#courses_list").html(addedCourseTemplate(courses_data));
 }
 
 function addedCourseTemplate(courses){
+    console.log(courses);
     let html_template = ``;
     
    for(let course = 0; course < courses.length; course++) {
-        html_template += `<div class="course">`;
-        html_template += `   <h5>${courses[course].course_title}</h5>`;
-        html_template += `   <table>`;
-        html_template += `       <tbody>`;
-      
-        for(let assignment = 0; assignment < courses[course].assignments.length; assignment++) {
-            html_template += `           <tr>`;
-            html_template += `               <td>${courses[course].assignments[assignment]}</td>`;
-            
-            for(let student = 0; student < students_data.length; student++) {
-                html_template += `            <td>--</td>`;
-            }
-            
-            html_template += `           </tr>`;
-        }
+       
+    if (courses[course].is_selected){
+           html_template += `<div class="course">`;
+           html_template += `   <h5>${courses[course].course_title}</h5>`;
+           html_template += `   <table>`;
+           html_template += `       <tbody>`;
 
-        html_template += `         </tbody>`;
-        html_template += `   </table>`;
-        html_template += `</div>`;
+           for(let assignment = 0; assignment < courses[course].assignments.length; assignment++) {
+               html_template += `           <tr>`;
+               html_template += `               <td>${courses[course].assignments[assignment]}</td>`;
+                
+               for(let student = 0; student < students_data.length; student++) {
+                   html_template += `            <td>--</td>`;
+                }
+                
+               html_template += `           </tr>`;
+            }
+
+            html_template += `         </tbody>`;
+            html_template += `   </table>`;
+            html_template += `</div>`;
+       }
    }
 
    return html_template;
